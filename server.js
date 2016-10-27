f5695b1c5d545ca69341ea3d153c45bda58370bb
Elenice Assis <elenicecassis@gmail.com>
2016-10-27 21:43:12 -0200

    Integrated router with server

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 "use strict"
 
 const http = require('http')
-const host = 'localhost'
+const url = require('url')
+const hostname = 'localhost'
 const port = 9000
 
-http
-    .createServer(onRequest)
-    .listen(port, onStart)
-
-function onRequest(request, response) {
-    console.log('Request for: ' + request.url)
-    response.statusCode = 500
-    response.setHeader('Content-Type', 'text/plain')
-    response.end('Hello World')
+function start(route) {
+    http
+        .createServer(onRequest)
+        .listen(port, onStart)
+    
+    function onRequest(request, response) {
+        console.log('Request for: ' + request.url)
+        
+        let path = url.parse(request.url).path
+        route(path)
+        
+        response.statusCode = 200
+        response.setHeader('Content-Type', 'text/plain')
+        response.end('Hello World!')
+    }
 }
+
 
 function onStart() {
-    console.log(`Server started at http://${host}:${port}`)
+    console.log(`Server started at http://${hostname}:${port}`)
 }
+
+//Public API
+exports.start = start
